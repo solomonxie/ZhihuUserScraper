70f6d0328c466ef003feef69392c1ebd83ff2449
Solomon Xie <solomonxiewise@gmail.com>
2019-02-24 14:13:17 +0800

    <refactor> optimize proxies reuse

--- draft/proxyPool.py
+++ draft/proxyPool.py
@@ -5,29 +5,33 @@ import json
 from threading import Thread
 
 proxies = []
-invalid = []
 
 def get_ips(count=20):
-    url = 'http://127.0.0.1:8000?count={}'.format(count)
     try:
-        r = requests.get(url=url)
+        r = requests.get(url='http://127.0.0.1:8000?count={}'.format(count))
         return json.loads( r.text )
     except Exception as e:
-        print('[ERR]', e)
+        print('[ERR] Proxy Pool Server', e)
 
+def delete(proxy):
+    try:
+        r = requests.get(url='http://127.0.0.1:8000/delete?ip={}'.format(proxy['ip']))
+    except Exception as e:
+        print('[ERR] Proxy Pool Server', e)
 
 def validate(proxy):
     global proxies
+    headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36'}
+    url = 'https://baidu.com'
     try:
         # print('[TRY]', n, proxies[n])
-        r = requests.get('https://baidu.com', proxies=proxy, timeout=3)
+        r = requests.get(url=url, headers=headers, proxies=proxy, timeout=2)
         proxies.append(proxy)
     except Exception as e:
         # print(e)
         # print('[INVALID]', proxy)
-        r = requests.get(url='http://127.0.0.1:8000/delete?ip={}'.format(proxy['ip']))
+        delete(proxy)
         pass
-
 
 def filter_valid():
     proxies = [parse_proxy(p) for p in get_ips(10)]
@@ -39,6 +43,7 @@ def filter_valid():
         t.start()
     for t in threads:
         t.join()
+    # print('[VALID-IPs]', len(proxies))
 
 
 def parse_proxy(d):
@@ -47,16 +52,18 @@ def parse_proxy(d):
         'ip': d[0]
     }
 
-def get_proxy():
-    filter_valid()
+def get():
     global proxies
-    # print('[VALID-IPs]', len(proxies))
-    ip = random.choice(proxies)
-    print( '[SWITCH-IP]', ip )
-    return ip
+    if proxies:   # Directly return IP in the pool
+        ip = proxies.pop(0)
+        print( '[USING-IP]', ip )
+        return ip
+    else:  # Retrive when pool's empty
+        filter_valid()
+        return get()
 
 
 if __name__ == '__main__':
     # for i in range(20):
         # get_valid_proxy()
-    print('[PROXY]', get_proxy())
+    print('[PROXY]', get())
